a78d56f007eb31cd45a1bb5677f191e13180c8df
Jay-Dholariya <jay.dholariya@brainvire.com>
2024-06-14 13:16:21 +0530

    perform file validation in Post model instead of validation in form

--- Social_Media_App/models.py
+++ Social_Media_App/models.py
@@ -3,17 +3,26 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import AbstractUser
 from phonenumber_field.modelfields import PhoneNumberField
+from os.path import splitext
+from django.core.exceptions import ValidationError
+
 
 class CustomUser(AbstractUser):
     mobile=PhoneNumberField(null=True, blank=True)
+
+def validated_file(value):
+    extentions = splitext(value.name)[1]
+    valid_extentions = [".pdf",".jpeg",".png"]
+    if extentions.lower() not in valid_extentions :
+        raise ValidationError("this file format is not supported")
     
-
-
-
+    file_size = value.size
+    if file_size>3*1024*1024: #2MB
+        raise ValidationError("the file size is very big")
 
 class Post(models.Model):
     title=models.CharField()
-    image=models.FileField(upload_to="Pictures",null=True)
+    image=models.FileField(upload_to="Pictures",null=True,blank=True,validators=[validated_file])
     content=models.TextField(null=True, blank=True)
     tag=models.CharField()
     posted_at=models.DateTimeField(auto_now_add=True)
